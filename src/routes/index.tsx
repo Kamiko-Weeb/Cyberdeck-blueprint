397ddb72237f61c9ea3e4de5fa94af18eb847eff
Kamiko <178470084+Kamiko-Weeb@users.noreply.github.com>
2026-09-13 11:51:05 -0700

    Convert to static Vite site

--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { DeckApp } from "@/components/deck-app";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <DeckApp />;
-}
